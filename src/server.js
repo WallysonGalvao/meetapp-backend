86e46fa469085839b87f35ff01a9fcef2985b020
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-10-11 11:16:44 -0300

    ESLint, Prettier & EditorConfig

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const app = require("./app");
+import app from './app';
 
 app.listen({ port: 3333 }, () =>
-  console.info(`❤ server this my love online...`, `http://localhost:3333/`)
+    console.info(`❤ server this my love online...`, `http://localhost:3333/`)
 );
