36d1fb0aa64cfd2ca0b23118179b67893945f1c3
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-10-11 12:28:07 -0300

    Cadastro de usuários

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true
-  },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        es6: true,
+        node: true,
     },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  rules: {
-    "prettier/prettier": "error",
-    "class-mothods-use-this": "off",
-    "no-params-reassign": "off",
-    camelcase: "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }]
-  }
+    extends: ['airbnb-base', 'prettier'],
+    plugins: ['prettier'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'class-methods-use-this': 'off',
+        'no-param-reassingn': 'off',
+        camelcase: 'off',
+        'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    },
 };
